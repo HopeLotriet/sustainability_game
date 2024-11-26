cad9c2350e9f15916fd42cc9c26fcb5e75429c4a
HopeLotriet <hopelotriet@gmail.com>
2024-11-26 14:03:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Survey from './components/Survey';
+import GameSelection from './components/GameSelection';
+import Results from './components/Results';
+import Leaderboard from './components/Leaderboard';
+import Profile from './components/Profile';
+import Logout from './components/Logout';
+import ProtectedRoute from './components/ProtectedRoute';
+import SDGGame from './components/games/SDGGame';
+import Learn from './components/games/Learn';
+import Quiz from './components/games/Quiz';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/survey" element={<ProtectedRoute><Survey /></ProtectedRoute>} />
+        <Route path="/games" element={<ProtectedRoute><GameSelection /></ProtectedRoute>} />
+        <Route path="/results" element={<ProtectedRoute><Results /></ProtectedRoute>} />
+        <Route path="/leaderboard" element={<ProtectedRoute><Leaderboard /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/games/learn/:sdgId" element={<ProtectedRoute><Learn /></ProtectedRoute>} />
+        <Route path="/games/quiz/:sdgId" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+        <Route path="/games/sdgGame/:sdgId" element={<ProtectedRoute><SDGGame /></ProtectedRoute>} />
+      </Routes>
+    </Router>
   );
 }
 
